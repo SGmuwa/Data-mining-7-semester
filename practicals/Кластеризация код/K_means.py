a9a82f52d14ae250e841a01be137b71c1592cc47
SG_Muwa <motherlode.muwa@gmail.com>
2020-01-22 18:42:21 +0300

    Уменьшение изучения

--- practicals/Кластеризация код/K_means.py
+++ practicals/Кластеризация код/K_means.py
@@ -1,52 +1,22 @@
-# To add a new cell, type '# %%'
-# To add a new markdown cell, type '# %% [markdown]'
-# %%
-import numpy as np
+﻿import numpy as np
 import pandas as pd
 number_of_clusters = 4
-#Чтение данных из файла
+# Чтение данных из файла
 clustered_elements=[]
-f = open("data_for_k_means", encoding = 'utf-8')
+f = open("data_for_k_means.csv", encoding="utf-8-sig")
 for line in f:
-    l = line.split(' ')
+    l = line.strip("\n").split("\t")
     for i in range(len(l)):
-        try:
-            l[i] = int(l[i].strip(" ").lower().strip("\n"))
-        except ValueError:
-            l[i] = float(l[i].strip(" ").lower().strip("\n"))
+        l[i] = float(l[i])
     clustered_elements.append(l)
 data = np.array(clustered_elements)
-print(data)
+print("Данные", data)
 
-
-# %%
-#Нормализация данных (Представление в виде от 0 до 1):  x_normed = (x - x.min(0)) / x.ptp(0)
-# print(np.ptp(data, axis=0))
-# for i in range(data.shape[1]):
-#     data[:,i] =( data[:,i] - data[:,i].min() ) / np.ptp(data, axis=0)[i] #все строки i-ого столбца
-# print(data)
-
-#для numpy
-# for i in range(data.shape[1]): #Цикл по столбцам
-#     average_value = np.mean(data[:,i]) #среднее значение столбца (np.mean(a) #среднее арифметическое)
-#     standard_deviation = np.std(data[:,i]) # std = sqrt(mean(abs(x - x.mean())**2))   Стандартное отклонение представляет собой квадратный корень из среднего значения квадратов отклонений от среднего значения
-#     data[:,i] = (data[:,i] -  average_value) /  standard_deviation
-# print(data)
-
-
-# %%
-#ТЕСТ на нормализацию 
-mean = data.mean(axis = 0)
-std = data.std(axis=0)
-data-=mean
-data /= std
-print(data)
 
 data.mean(axis = 0)
 data.std(axis = 0)
 
 
-# %%
 def get_euclidean_distance (l1,l2):
     euclid = 0
     count = len(l1)
@@ -56,9 +26,9 @@ def get_euclidean_distance (l1,l2):
 
 list_for_serch_start_centers = []
 
-#Центроид для всех данных:
-centroid = (data.sum(axis = 0))/data.shape[0]
-print(centroid)
+# Центроид для всех данных:
+centroid = (data.sum(axis = 0)) / data.shape[0]
+print("Центроид", centroid)
 
 #Ищем расстояние каждого элемента до центра:
 for i in range(len(data)):
@@ -75,13 +45,11 @@ for i in range(number_of_clusters):
 print(ind_centers)
 
 
-# %%
 # Алгоритм К-средних
 import random
 ind_centers = random.sample(list(range(data.shape[0])), number_of_clusters)
 cluster_numbers = list(range(number_of_clusters))#Номера кластеров
-print(ind_centers)
-#ind_centers = [18, 1, 8, 3]
+print("ind_centers", ind_centers)
 centers = []
 for i in ind_centers:
     centers.append((data[i,:]).tolist())
@@ -152,79 +120,5 @@ while True:
     if stop_criterion(l_centroid, centers) : break
     centers = l_centroid.copy()
     print("------------------------------------------------------------------------------------------------------------")
-
-
-# %%
-#ТЕСТ
-print((-1.32911564+(-1.32911564))/2)
-el = [1,[1,7]]
-m = el[1]
-num_center_of_cl = data.shape[1] #число координат центра
-center_of_cl = []
-    
-for i in range(num_center_of_cl):#Для каждок координаты центра
-    temp_coord = 0 
-    for j in m:
-        print("data[j,i]",data[j,i])
-        temp_coord+=data[j,i]
-    temp_coord /= len(m)
-    center_of_cl.append(temp_coord)
-print(center_of_cl)
-[0.20865709 -1.24771194 -0.93799953 -1.32911564]
-[-0.17977678 -1.61740437 -1.14750196 -1.32911564]
-
-
-# %%
-import numpy as np
-x = np.array([[0, 1], 
-              [2, 3]]) 
-
-print(np.ptp(x, axis=0)) #0 - ось столбцов np.ptp(x, axis=1) #0 - ось столбцов
-
-x[:,1] =( x[:,1] - x[:,1].min() ) / np.ptp(x, axis=0)[1] #все строки i-ого столбца
-print(x)
-
-
-# %%
-b = np.array([[1, 2, 3], 
-              [4, 5, 6]])
-for i in range(b.shape[1]): #Цикл по столбцам
-    average_value = np.mean(b[:,i]) #среднее значение столбца (np.mean(a) #среднее арифметическое)
-    standard_deviation = np.std(b[:,i]) # std = sqrt(mean(abs(x - x.mean())**2))   Стандартное отклонение представляет собой квадратный корень из среднего значения квадратов отклонений от среднего значения
-    b[:,i] = (b[:,i] -  average_value) /  standard_deviation
-print(b)
-
-
-for i in b:
-    print(type(i))
-
-
-# %%
-center_of_cl = [None]*5
-len(center_of_cl)
-
-
-# %%
-b = [[1, 2, 3], [4, 5, 6]]
-a = b.copy()
-print(a)
-#Списки равны, если элементы с одинаковым индексом равны. Учитывается порядок.
-print("a==b",a==b)
-aaaaaaaaa = []
-print("c==[]",len(aaaaaaaaa)==0)
-print("a==c",a==c)
-
-
-# %%
-g1 = [[0.5842459288630132, 0.41590398067525874, 0.4761418927616714, -0.3271032249022722], [0.014440155331363919, -1.4325581556592244, -1.0427507451480607, -1.3291156353623974], [-0.33136532628503307, -0.7034425352161782, -0.7168580718800721, -0.23601118758771536], [-0.11041358945510202, 1.3401350488425003, 1.1360745561293482, 1.3489902616855738]]
-g2 = [ [0.014440155331363919, -1.4325581556592244, -1.0427507451480607, -1.3291156353623974], [-0.33136532628503307, -0.7034425352161782, -0.7168580718800721, -0.23601118758771536], [-0.11041358945510202, 1.3401350488425003, 1.1360745561293482, 1.3489902616855738]]
-print(g1==g2)
-student_tuples = [
-        ('john', 'A', 15),
-        ('jane', 'B', 12),
-        ('dave', 'B', 10),
-    ]
-sor = sorted(student_tuples, key=lambda student: student[2])   # сортируем по возрасту
-print(sor)
 
 
