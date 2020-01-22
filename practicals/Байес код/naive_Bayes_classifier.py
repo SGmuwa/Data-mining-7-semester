7f5b888b6f7724639d34f7a74a11061da69485fe
SG_Muwa <motherlode.muwa@gmail.com>
2020-01-22 15:11:36 +0300

    Отладка Байеса, исправлено представление данных

--- practicals/Байес код/naive_Bayes_classifier.py
+++ practicals/Байес код/naive_Bayes_classifier.py
@@ -2,23 +2,28 @@
 
 # Чтение данных из файла
 number_of_categorical_attribute = []
-train_data=[]
+data=[]
 with open(input("A filename: "), encoding = "utf-8") as file:
     number_of_categorical_attribute = file.readline().split(":")[1].split(",")
     target_var = int(file.readline().split(":")[1])
     number_of_classes = int(file.readline().split(":")[1])
     file.readline()
     for line in file:
-        l = line.split("\t")
+        l = line.replace('\n', '').split("\t")
         for i in range(len(l)):
-            l[i] = l[i].strip("\t").lower().strip("\n")
-        train_data.append(l)
-data = np.array(train_data)
+            try:
+                l[i] = int(l[i])
+            except ValueError:
+                try:
+                    l[i] = float(l[i])
+                except:
+                    l[i] = l[i]
+        data.append(l)
 print("Данные:\n", data)
 
 
-number_of_column = data.shape[1]
-number_of_data = data.shape[0]
+number_of_column = len(data[0])
+number_of_data = len(data)
 
 
 # Функция map принимает два аргумента: функцию и аргумент составного типа данных, например, список. 
@@ -33,9 +38,17 @@ print("Категориальные признаки: {0}\nЦелевая пер
 # Чтение гипотезы
 hypothesis_data = {}
 with open(input("H filename: "), encoding = "utf-8") as f:
-    line = f.readline().split("\t")
-    for num, attribute in zip(number_of_categorical_attribute, line):
-        hypothesis_data[num] = attribute
+    line = f.readline().replace('\n', '').split("\t")
+    attributes = list(range(number_of_column))
+    del attributes[target_var]
+    for num, attribute in zip(attributes, line):
+            try:
+                hypothesis_data[num] = int(attribute)
+            except ValueError:
+                try:
+                    hypothesis_data[num] = float(attribute)
+                except ValueError:  
+                    hypothesis_data[num] = attribute
                     
 print("Гипотеза: ", hypothesis_data)
 
@@ -43,47 +56,71 @@ print("Гипотеза: ", hypothesis_data)
 # Получение уникальных значений столбцов
 uniq_elements = []
 for i in range(number_of_column):
-    uniq_elements.append([i, np.unique(data[:,i], axis = 0)])
+    uniq_elements.append(set(d[i] for d in data))
 print(uniq_elements)
 
 
 count_el_of_class=[]
+target_column = tuple(d[target_var] for d in data)
 # Число объектов в разных классах
-for c in uniq_elements[target_var][1]:
-    count_el_of_class.append(list(data[:,target_var]).count(c))
+for c in uniq_elements[target_var]:
+    count_el_of_class.append(target_column.count(c))
 print("count_el_of_class", count_el_of_class)
 # Вычисляем априорную вероятность появления класса
 probability_classes = []
-for c in uniq_elements[target_var][1]:
-    probability_classes.append(len(list(filter(lambda x: x == c, data[:,target_var])))  / number_of_data )
+for c in uniq_elements[target_var]:
+    probability_classes.append(len(list(filter(lambda x: x == c, target_column)))  / number_of_data )
 print("Априорная вероятность появления класса", probability_classes)
 
 # Расчёт условных вероятностей
-def get_count_of_set_in_train_data(number_of_att, att, class_):
+def get_count_of_set_in_train_data(data, number_of_att, att, class_):
     count = 0
     for id, row_data in enumerate(data):
-        if(data[id, number_of_att] == att and data[id, target_var] == class_):
+        if(data[id][number_of_att] == att and data[id][target_var] == class_):
             count+=1
     return count
 
 conditional_probabilities = []
-for ind_cl,cl in enumerate(uniq_elements[target_var][1]): # Для каждого класса
+for ind_cl, cl in enumerate(uniq_elements[target_var]): # Для каждого класса
     cond_prob_for_class = []
     for att_ind in number_of_categorical_attribute: # Для категориальных атрибутов
-        cond_prob_for_class.append((get_count_of_set_in_train_data(att_ind, hypothesis_data[att_ind], cl)) / count_el_of_class[ind_cl])
+        cond_prob_for_class.append((get_count_of_set_in_train_data(data, att_ind, hypothesis_data[att_ind], cl)) / count_el_of_class[ind_cl])
     conditional_probabilities.append([cl, cond_prob_for_class])
-print(conditional_probabilities)
+print("conditional_probabilities", conditional_probabilities)
 
+#находим непрерыные атрибуты
+continuous_attributes = {}
+if len(number_of_categorical_attribute) + 1 < number_of_column:
+    continuous_attributes = set([x for x in range(number_of_column)])
+    continuous_attributes.difference_update(set(number_of_categorical_attribute))
+    continuous_attributes.difference_update({target_var})
+print(continuous_attributes)
 
+import math
+#Функция Гаусса
+def getvalue_func_Gauss(x,m,d):
+    return (1/((2*math.pi*d)**0.5)) * math.exp((-1)*( (x-m)**2  / 2*d))
 
 # среднее значение и дисперсия Атрибута Ai для наблюдений относящихся к классу Ск
 def get_dispersion_and_math_expected(column, cl):
     list_result = []
-    for i, el in enumerate(number_of_data):
-        if data[i,target_var] == cl:
-            list_result.append(data[i,column])
+    for i in range(number_of_data):
+        if data[i][target_var] == cl:
+            list_result.append(data[i][column])
+    print("list_result", list_result)
     return [np.mean(list_result), np.var(list_result)]  
 
+cond_prob_for_continuous_attributes = []
+if(continuous_attributes!={}):
+    for ind_cl,cl in enumerate(uniq_elements[target_var]):#Для каждого класса
+        cb_for_class = []
+        for c in continuous_attributes: # По столбцам (по непрерывным атрибутам)
+            l_m_d = get_dispersion_and_math_expected(c, cl)
+            print(hypothesis_data, c)
+            val = getvalue_func_Gauss(hypothesis_data[c],l_m_d[0],l_m_d[1])
+            cb_for_class.append(val)                               
+        cond_prob_for_continuous_attributes.append([cl,cb_for_class])
+print(cond_prob_for_continuous_attributes)
 
 
 # Произведение условная вероятность и вероятность класса
