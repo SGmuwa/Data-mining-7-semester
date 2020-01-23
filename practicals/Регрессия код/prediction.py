f492a8dd6cb3bf350fc27f4a9a3b06d5f2400c61
SG_Muwa <motherlode.muwa@gmail.com>
2020-01-23 13:29:14 +0300

    Форматирование регрессии

--- practicals/Регрессия код/prediction.py
+++ practicals/Регрессия код/prediction.py
@@ -21,14 +21,14 @@ print(data)
 b1 = 10 * np.sum(data[:, 0] * data[:, 1]) - (np.sum(data[:, 0]) * np.sum(data[:, 1]))
 b1 /= ((np.sum(data[:, 0] ** 2) * 10 - np.sum(data[:, 0]) ** 2))
 print("b1 = ", b1)
-# Находим b0=
+# Находим b0
 b0 = np.sum(data[:, 1]) / data.shape[0] - (b1 / data.shape[0]) * np.sum(data[:, 0])
 print("b0 = ", b0)
 
-x = np.linspace(data[:,0].min(), data[:,0].max())
-y =b0 + (b1*x)
+x = np.linspace(data[:, 0].min(), data[:, 0].max())
+y = b0 + (b1 * x)
 plt.title("График зависимости входной переменной X от Y") # заголовок
-plt.plot(data[:,0], data[:,1],"ro",x,y) # построение графика
+plt.plot(data[:, 0], data[:, 1], "ro", x, y) # построение графика
 plt.xlabel("x") # ось абсцисс
 plt.ylabel("y") # ось ординат
 plt.grid()      # включение отображение сетки
@@ -37,9 +37,9 @@ plt.show()
 
 # Вычислим значение y` предсказанное сетью и запишим данные и предсказанные значения в matrix
 def pred(x):
-    return b0+b1*x
+    return b0 + b1 * x
 array_y_pred = []
-for idx, item in enumerate(data[:,0]):
+for idx, item in enumerate(data[:, 0]):
     t = pred(item)
     array_y_pred.append([t,])
 matrix = np.hstack((data, np.array(array_y_pred)))
@@ -49,35 +49,35 @@ print(matrix)
 
 # Оценка соответствия простой линейной регрессии реальным данным
 # Стандартная ошибка оценки
-st_e = ((matrix[:,1] - matrix[:,2])**2).sum()
-st_e/=(data.shape[0] - m - 1)
-st_e = st_e**0.5
-print("Стандартная ошибка = ",st_e)
+st_e = ((matrix[:, 1] - matrix[:, 2]) ** 2).sum()
+st_e /= (data.shape[0] - m - 1)
+st_e = st_e ** 0.5
+print("Стандартная ошибка = ", st_e)
 
-mean_y = np.mean(data, axis=0) [1] #mean_y = np.mean(data, axis=0)   -  среднии значения столбцов
+mean_y = np.mean(data, axis=0)[1] #mean_y = np.mean(data, axis=0)   -  среднии значения столбцов
 e_st_mean = ((1 / (data.shape[0] - 1)) * ((matrix[:, 1] - mean_y) ** 2).sum()) ** 0.5
 print("Ошибка относительно среднего значения = ", e_st_mean)
 print("Отношение", e_st_mean / st_e)
 
 
-#три квадратичные суммы
-#общая полная
+# три квадратичные суммы
+# общая полная
 Q = ((matrix[:, 1] - mean_y) ** 2).sum()
 Q_r = ((matrix[:, 2] - mean_y) ** 2).sum()
 Q_e = ((matrix[:, 1] - matrix[:, 2]) ** 2).sum()
-print(Q_r,Q_e)
-print("Q = Q_r + Q_e:  ", Q, " = ", Q_r + Q_e)
+print("Q = ", Q)
+if Q_r + Q_e != Q: raise NameError("Q != Q_r + Q_e", Q, " != ", Q_r + Q_e)
 
 
-#Коэффициент детерминации
-r_squared = Q_r/Q
-print("Коэффициент детерминации",r_squared)
+# Коэффициент детерминации
+r_squared = Q_r / Q
+print("Коэффициент детерминации", r_squared)
 
 
 #Коэффициент корреляции
 x_mean = np.mean(data, axis = 0)[0]
-std_x = np.std(data[:,0]) # std = sqrt(mean(abs(x - x.mean()) ** 2)) 
-std_y = np.std(data[:,1])
+std_x = np.std(data[:, 0]) # std = sqrt(mean(abs(x - x.mean()) ** 2)) 
+std_y = np.std(data[:, 1])
 r = ((data[:, 0] - x_mean) * (data[:, 1] - mean_y)).sum()
 r /= ((data.shape[0]) * std_x * std_y)
 print("Коэффициент корреляции = ", r)
